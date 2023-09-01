7012542fd5ca9f3e3b1015c07a0e4abf647613ef
Mikelantium <leugimmail@gmail.com>
2023-09-01 10:23:20 +0200

    invoices

--- front_farm/src/Context/Action.jsx
+++ front_farm/src/Context/Action.jsx
@@ -16,10 +16,12 @@ export async function loginUser(dispatch, loginPayload) {
       dispatch({ type: 'LOGIN_SUCCESS', payload: data });
       localStorage.setItem('user', JSON.stringify(data.user));
       localStorage.setItem('token', data.token);
+      console.log(data);
       return data
     }
- 
+    
     dispatch({ type: 'LOGIN_ERROR', error: data.errors[0] });
+    
     return;
   } catch (error) {
     dispatch({ type: 'LOGIN_ERROR', error: error });
